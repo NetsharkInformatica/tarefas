d539aef54cc804c33f15c65fac361bd46bf51c68
Hélio Tomé <helio.tome2024@gmail.com>
2024-12-12 10:13:38 -0300

    projeto finalizado

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ janela.config(bg="#F0F0F0")
 '''dimensoes da minha janela'''
 janela.geometry("600x650")
 '''criar cabeçalho'''
-fonte_cabecalho=font.Font(family="Roboto",size=24, weight="bold")
+fonte_cabecalho=font.Font(family="Arial",size=24, weight="bold")
 rotulo_cabecalho= tk.Label(janela,text="meu app de tarefas",
                            font=fonte_cabecalho,bg="#f0f0f0",fg="#333").pack(padx=10)
 
@@ -37,7 +37,7 @@ icon_deletar=PhotoImage(file="imgs/delete.png").subsample(20,20)
          
 def adicionar_item_tarefa(tarefa):
     frame_tarefa=tk.Frame(canvas_interior,bg="#fff",bd=1,relief=tk.SOLID)
-    label_tarefa=tk.Label(frame_tarefa,text=tarefa,font=("roboto",16),bg="#fff",width=30,height=2,anchor="w")
+    label_tarefa=tk.Label(frame_tarefa,text=tarefa,font=("Arial",16),bg="#fff",width=30,height=2,anchor="w")
     label_tarefa.pack(side=tk.LEFT,padx=10,pady=5,fill=tk.X)
     
     botao_edicao=tk.Button(frame_tarefa,image=icon_editar,command=lambda 
@@ -59,15 +59,36 @@ def adicionar_item_tarefa(tarefa):
     
     
     
+def preparar_edicao(frame_tarefa,label_tarefa):
+    global frame_em_edicao
+    frame_em_edicao= frame_tarefa
+    entrada_tarefa.delete(0,tk.END)
+    entrada_tarefa.insert(0,label_tarefa.cget("text"))
+    
 
+def atualizar_tarefa(nova_tarefa):
+    global frame_em_edicao
+    for widget in frame_em_edicao.winfo_children():
+        if isinstance(widget,tk.Label):
+            widget.config(text=nova_tarefa)
+        
+   
 
+def deletar_tarefa(frame_tarefa):
+    frame_tarefa.destroy()
+    canvas_interior.update_idletasks()
+    canvas.config(scrollregion=canvas.bbox("all"))
+    
 
-
-
-
-def atualizar_tarefa():
-    pass
-            
+def alternar_sublinhado(label):
+    fonte_atual = label.cget("font")
+    if "overstrike" in fonte_atual:
+        nova_fonte=fonte_atual.replace(" overstrike","")
+    else:
+        nova_fonte=fonte_atual + " overstrike"
+    label.config(font=nova_fonte)
+    
+       
 
 #criando o frame
 
@@ -76,14 +97,14 @@ frame.pack(padx=10)
 
 #criar local de entrada da tarefa no frame
 
-entrada_tarefa=tk.Entry(frame,font=("Roboto",14),relief=tk.FLAT,bg="#fff",fg="gray",width=30)
+entrada_tarefa=tk.Entry(frame,font=("Arial",14),relief=tk.FLAT,bg="#fff",fg="gray",width=30)
 entrada_tarefa.pack(side=tk.LEFT,pady=10,padx=10)
 
 #criar botao adicionar tarefa
 
 botao_adicionar=tk.Button(frame,command=adicionar_tarefa,text="adicionar tarefa",bg="#4caf50",
                           fg="#FFF",height=1,width=15,
-                          font=("Garamond",15),relief=tk.FLAT,padx=10)
+                          font=("Arial",15),relief=tk.FLAT,padx=10)
 botao_adicionar.pack(padx=10,side=tk.LEFT)
 
 #criar um frame para lista de tarefas com rolagem
